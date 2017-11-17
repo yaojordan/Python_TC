2d77e5c4a605bae4fab26e958d4c0e54de25d490
宇鴻 <yaojordan@gmail.com>
2017-11-17 09:42:24 +0800

    Update del_set.py

--- del_set.py
+++ del_set.py
@@ -6,26 +6,46 @@ import shutil
 
 
 def delete(ini_file):
-    with open(ini_file, 'r+') as f:
-        lines = f.readlines()#read all lines
-        #print(lines)
-        if sys.argv[1] == "TC304":
-            for i, line in enumerate(lines):
-                if line.startswith("-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ="):
-                    lines[i] = lines[i].replace("true", "false")
-                    lines.pop()
-        else:
-            lines.pop()#Delete last line
-    f.close()
+	with open(ini_file, 'r+') as f:
+		lines = f.readlines()#read all lines
+		#print(lines)
 
-    with open(ini_file, 'w') as f:
-        #print(lines)
-        f.writelines(lines)
-    f.close()
+		if sys.argv[1] == "TC304":
+			for i, line in enumerate(lines):
+				if line.startswith("-XXX="):
+					lines[i] = lines[i].replace("true", "false")
+					lines.pop()
+
+		elif sys.argv[1] == "TC315":
+			for i, line in enumerate(lines):
+				if line.startswith("-XXX=false"):
+					lines[i] = lines[i].replace("-XXX=false", "")
+					lines.pop()
+					
+		elif sys.argv[1] == "TC348":
+			for i, line in enumerate(lines):
+				if line.startswith("-XXX=XXX"):
+					lines[i] = lines[i].replace("-XXX=XXX", "")
+					lines.pop()
+					
+		elif sys.argv[1] == "TC206":
+			for i, line in enumerate(lines):
+				if line.startswith("-XXX=http://xxx/checker.php"):
+					lines[i] = lines[i].replace("-XXX=http://xxx/checker.php", "")
+					lines.pop()
+		else:
+			lines.pop()#Delete last line
+					
+	f.close()
+
+	with open(ini_file, 'w') as f:
+		#print(lines)
+		f.writelines(lines)
+	f.close()
 
 try:
-    delete(r'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ')
+	delete(r'XXXXXXXXXXXXXXXXX')
 
 except Exception as e:
-    print(e)
-    sys.exit(1)
+	print(e)
+	sys.exit(1)
